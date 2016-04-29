3db07fba98fa660fdbe78859ce12d251765a0d80
vicmatmar <victormartin@hotmail.com>
2016-04-29 15:02:10 -0500

    Simple app showing how to use ftbutton class

---
+++ FTButtonApp/FTButton.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace FT32HDIO
+{
+    /// <summary>
+    /// Class use to monitor a hardware push button attached to FT232H board.
+    /// It is assume a pull-up resistor is at the FT232H pin and the switch connects it to ground.
+    /// The button default state is enabled but as soon as a click is detected it gets disabled and the
+    /// click event fires to avoid double clicks and triggering unwanted events.
+    /// This class uses a while loop and thread.sleep to data pool the pin where the switch is connected to.
+    /// </summary>
+    class FTButton
+    {
+        ft232hdio _dio;
+        ft232hdio.DIO_BUS _bus = ft232hdio.DIO_BUS.AC_BUS;
+        ft232hdio.PIN _pin = ft232hdio.PIN.PIN0;
+
+        public ft232hdio DIO { get { return _dio; } }
+        public ft232hdio.DIO_BUS BUS { get { return _bus; } }
+        public ft232hdio.PIN PIN { get { return _pin; } }
+
+        public delegate void ClickHandler(object sender);
+        public event ClickHandler Click_Event;
+
+        public bool Enabled { get { return _enable; } set { _enable = value; } }
+
+        bool _enable = false;
+        int _sampling_ms = 150;
+
+        Task _task_monitor;
+        CancellationTokenSource _cancel_token = new CancellationTokenSource();
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="dio"></param>
+        /// <param name="bus"></param>
+        /// <param name="pin"></param>
+        public FTButton(ft232hdio dio, ft232hdio.DIO_BUS bus, ft232hdio.PIN pin)
+        {
+            _dio = dio;
+            _bus = bus;
+            _pin = pin;
+
+            Enabled = true;
+
+            _task_monitor = new Task(() => monitor(_cancel_token.Token), _cancel_token.Token);
+            //_task_monitor.ContinueWith(pretest_done_handler, TaskContinuationOptions.OnlyOnRanToCompletion);
+            _task_monitor.ContinueWith(monitor_exception_handler, TaskContinuationOptions.OnlyOnFaulted);
+            _task_monitor.Start();
+
+
+        }
+
+        /// <summary>
+        /// Loop used to monitor pin state
+        /// When pin is low the sampling is disabled and the click event fires
+        /// </summary>
+        void monitor(CancellationToken cancel)
+        {
+            while (true)
+            {
+                if (cancel.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                if (Enabled)
+                {
+                    bool value = get_button_state();
+                    // Assume state is always high (pull up resistor) and
+                    // button press sets it low (GND)
+                    if (!value)
+                    {
+                        Enabled = false;
+                        fire_click();
+                    }
+                }
+                Thread.Sleep(_sampling_ms);
+            }
+        }
+
+        void monitor_exception_handler(Task task)
+        {
+            var exception = task.Exception;
+            string errmsg = exception.InnerException.Message;
+            throw new Exception(errmsg);
+        }
+
+
+        /// <summary>
+        /// Gets pin value
+        /// </summary>
+        /// <returns></returns>
+        private bool get_button_state()
+        {
+            bool value = _dio.ReadPin(_bus, _pin);
+            return value;
+        }
+
+        /// <summary>
+        /// Fire the event
+        /// </summary>
+        void fire_click()
+        {
+            if (Click_Event != null)
+            {
+                Click_Event(this);
+            }
+        }
+
+    }
+}
---
+++ FTButtonApp/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+using FT32HDIO;
+
+namespace FTButtonApp
+{
+    /// <summary>
+    /// Sample app that demonstrates the use of the FTButton and
+    /// controls LEDs
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        ft232hdio _ftdio; // the dio controller
+        FTButton _ftbutton; // the ftbutton switch
+        delegate void setCallback(); // used to update the gui if InvokeRequired
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            // Open the ft232h controller
+            _ftdio = new ft232hdio();
+            int dev_index = _ftdio.GetFirstDevIndex();
+            if (dev_index < 0)
+            {
+                throw new Exception("Unable to find an F232H device");
+            }
+            _ftdio.Open((uint)dev_index);
+
+
+            // Create the ftbutton
+            _ftbutton = new FTButton(_ftdio, ft232hdio.DIO_BUS.AD_BUS, ft232hdio.PIN.PIN0);
+            // Subscribe to click event
+            _ftbutton.Click_Event += _ftbutton_Click_Event;
+
+        }
+
+        /// <summary>
+        /// Init gui state on form load
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Init gui
+            updategui();
+        }
+
+        /// <summary>
+        /// Handles the button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        void _ftbutton_Click_Event(object sender)
+        {
+            // The button should now be disabled
+            // Report something is running...
+            updategui();
+
+            // Just pretend something is running for some time
+            Thread.Sleep(5000);
+
+            // Now enable the button so a new click can be detected
+            _ftbutton.Enabled = true;
+
+            // Report we are idle
+            updategui();
+        }
+
+        /// <summary>
+        /// Update the gui depending on the enablement state of the ftbutton
+        /// We asume somthing is running when it is enable and idle when disable
+        /// </summary>
+        void updategui()
+        {
+            if (InvokeRequired)
+            {
+                setCallback d = new setCallback(updategui);
+                this.Invoke(d, new object[] { });
+            }
+            else
+            {
+                if (_ftbutton.Enabled)
+                {
+                    label1.Text = "Waiting on button click.\r\nButton is enabled.\r\nLED is on";
+
+                    // Toggle leds as if something passed or failed
+                    Random ran = new Random();
+                    bool pass_fail = Convert.ToBoolean(ran.Next(2));
+                    turn_leds_pass_fail(pass_fail);
+                    radioButton1.Checked = true;
+                }
+                else
+                {
+                    label1.Text = "Something running.\r\nButton is disabled\r\nLED is off";
+
+                    turn_leds(false);
+                    radioButton1.Checked = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Turn either the green or red LEDs
+        /// </summary>
+        /// <param name="pass_fail"></param>
+        void turn_leds_pass_fail(bool pass_fail)
+        {
+            if (pass_fail)
+            {
+                _ftdio.SetPin(ft232hdio.DIO_BUS.AC_BUS, ft232hdio.PIN.PIN0, true);
+                _ftdio.SetPin(ft232hdio.DIO_BUS.AC_BUS, ft232hdio.PIN.PIN2, true);
+            }
+            else
+            {
+                _ftdio.SetPin(ft232hdio.DIO_BUS.AC_BUS, ft232hdio.PIN.PIN1, true);
+                _ftdio.SetPin(ft232hdio.DIO_BUS.AC_BUS, ft232hdio.PIN.PIN3, true);
+            }
+        }
+
+        /// <summary>
+        /// Turn all LEDs
+        /// </summary>
+        /// <param name="state"></param>
+        void turn_leds(bool state)
+        {
+            _ftdio.SetPin(ft232hdio.DIO_BUS.AC_BUS, ft232hdio.PIN.PIN0, state);
+            _ftdio.SetPin(ft232hdio.DIO_BUS.AC_BUS, ft232hdio.PIN.PIN1, state);
+            _ftdio.SetPin(ft232hdio.DIO_BUS.AC_BUS, ft232hdio.PIN.PIN2, state);
+            _ftdio.SetPin(ft232hdio.DIO_BUS.AC_BUS, ft232hdio.PIN.PIN3, state);
+        }
+
+    }
+}
